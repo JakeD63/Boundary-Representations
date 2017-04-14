c5da9189b5e93b7506e1133f034d95fe178affaa
Jake <jake.davidson@mines.sdsmt.edu>
2017-04-14 12:58:01 -0600

    genChainCode function

--- ChainCode.cpp
+++ ChainCode.cpp
@@ -3,6 +3,15 @@
 //
 
 #include "ChainCode.h"
+
+ChainCode::ChainCode(cv::Mat img) : shape2D(img){
+	this->chainCode = genChainCode();
+}
+
+//use boundary to generate chain code
+//algorithm described on page 800 of textbook
 std::vector<int> ChainCode::genChainCode() {
-	
+	for (Point i : boundary) {
+		cout << i.x << ", " << i.y << endl;
+	}
 }
