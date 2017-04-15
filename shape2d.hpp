95b65638f5fd695e669d6c6ad35c420af99dff80
Jake Davidson <jake.davidson@mines.sdsmt.edu>
2017-04-15 14:35:58 -0600

    moved min and max to protected to be used in child class

--- shape2d.hpp
+++ shape2d.hpp
@@ -10,15 +10,16 @@
 
 class shape2D {
 	private:
-		unsigned int max_x, max_y, min_x, min_y;
 		cv::Point find_tm_lm(cv::Mat img);
 		void rotate_cw(const cv::Point p, cv::Point& c_n);
 		void find_next(cv::Point& p, cv::Point& c_n);
 
 	protected:
 		std::vector<cv::Point> boundary;
+		unsigned int max_x, max_y, min_x, min_y;
 
-	public:
+
+public:
 		shape2D(cv::Mat img);
 		~shape2D();
 
