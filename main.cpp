d801fd67af0a5d228e76c3f37053297635f99c27
Jake Davidson <jake.davidson@mines.sdsmt.edu>
2017-04-15 17:30:08 -0600

    new short program to compare two shape number outputs

--- main.cpp
+++ main.cpp
@@ -1,40 +1,41 @@
 #include <opencv2/opencv.hpp>
-
-#include "shape2d.hpp"
 #include "ShapeNumber.hpp"
 
 using namespace std;
 using namespace cv;
 
-void compare(Mat img, int scale);
+void compare(Mat img1, Mat img2, int scale);
 
 int main(int argc, char** argv)
 {
-	if(argc != 3) {
-		cerr << "Usage: dip3 image gridScale" << endl;
+	if(argc != 4) {
+		cerr << "Usage: dip3 image1 image2 gridScale" << endl;
 		exit(0);
 	}
 	// Load the image
-	Mat src = imread(argv[1]);
+	Mat img1 = imread(argv[1]);
+	Mat img2 = imread(argv[2]);
 	
 	// Check if everything was fine
-	if(!src.data)
+	if(!img1.data || !img2.data)
 		return -1;
 
-	compare(src, atoi(argv[2]));
+	compare(img1, img2, atoi(argv[3]));
 	return 0;
 }
 
-void compare(Mat src, int scale) {
-	cvtColor(src, src, CV_BGR2GRAY);
-	Mat bin_img;
-	threshold(src, bin_img, 128, 255, 1);
-	Mat orig = shape2D(bin_img).to_mat();
+void compare(Mat img1, Mat img2, int scale) {
+	Mat bin_img1, bin_img2;
 
-	auto s = ShapeNumber(bin_img, scale);
-	Mat out = s.to_mat();
+	cvtColor(img1, img1, CV_BGR2GRAY);
+	cvtColor(img2, img2, CV_BGR2GRAY);
+	threshold(img1, bin_img1, 128, 255, 1);
+	threshold(img2, bin_img2, 128, 255, 1);
 
-	imshow("Bound_Orig", orig);
-	imshow("SN_Bound", out);
+	Mat out1 = ShapeNumber(bin_img1).to_mat();
+	Mat out2= ShapeNumber(bin_img2, scale).to_mat();
+
+	imshow("Image1", out1);
+	imshow("Image2", out2);
 	waitKey(0);
 }
