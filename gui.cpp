d3db4139e6e4c5d3ac4670376eac2654431ef8f6
Lawence Hoffman <lawrence@lawrencehoffman.net>
2017-04-23 22:04:14 -0600

    Minor Fixups

--- gui.cpp
+++ gui.cpp
@@ -29,3 +29,38 @@ void showSnGui(int trac, void* data) {
 	imshow(temp->wName, temp->sn.to_mat());
 	cout << "callback finished" << endl;
 }
+
+/**
+ * This struct is a way to get out of using global variables to controll the
+ * fourier descriptor window
+ */
+struct fd_gui {
+	int desc_count, max_d, wHeight, wWidth;
+	string wName;
+	FourierDescriptor fd;
+	
+	fd_gui(Mat img, int height, int width, string name) : 
+		fd(img),
+		wHeight(height),
+		wWidth(width),
+		wName(name)
+	{
+		max_d = fd.size();
+		desc_count = (int) floor(max_d / 2);
+		namedWindow(wName);
+		resizeWindow(wName, wWidth, wHeight);
+	};
+
+};	
+
+/**
+ * This is the callback function called by the trackbar to update the fourier
+ * descriptor window
+ */
+void showFdGui(int trac, void* data)
+{
+	fd_gui* temp = (fd_gui*) data;
+	temp->fd.reconstruct(trac);
+	imshow(temp->wName, temp->fd.to_mat());
+}
+
--- main.cpp
+++ main.cpp
@@ -6,88 +6,45 @@
 #include "fourier_desc.hpp"
 #include "ShapeNumber.hpp"
 
+#include "gui.cpp"
+
 using namespace std;
 using namespace cv;
 
 const string HELP = "usage: ./dip3 <imagefile>";
 
-void showFdGui(int trac, void* data);
-
-void erodeToSingle(Mat bimg, Mat& res, int thresh)
+/**
+ * Simply convert color and threshold the image
+ */
+void preProcess(Mat bimg, Mat& res)
 {
-	Mat element = getStructuringElement( MORPH_RECT,
-			Size( 7, 7 ),
-			Point( 3, 3) );
-
-	bool done = false;
-	do {
-		erode(bimg, res, element);
-
-		FourierDescriptor fd(res);
-		if( fd.size() > thresh ) 
-			return;
-
-	} while ( !done );
-
-
-}
-
-
-struct fd_gui {
-	int desc_count, max_d, wHeight, wWidth;
-	string wName;
-	FourierDescriptor fd;
-	
-	fd_gui(Mat img, int height, int width, string name) : 
-		fd(img),
-		wHeight(height),
-		wWidth(width),
-		wName(name)
-	{
-		max_d = fd.size();
-		desc_count = (int) floor(max_d / 2);
-		namedWindow(wName);
-		resizeWindow(wName, wWidth, wHeight);
-	};
-
-};	
-
-void showFdGui(int trac, void* data)
-{
-	fd_gui* temp = (fd_gui*) data;
-	temp->fd.reconstruct(trac);
-	imshow(temp->wName, temp->fd.to_mat());
+	cvtColor(bimg, bimg, CV_BGR2GRAY);
+	threshold(bimg, res, 128, 255, 1);
 }
 
 int main(int argc, char **argv)
 {
-	string testW = string("TEST");
+	string fdWin = string("Fourier Descriptors");
 	if ( argc < 2 )
 	{
 		cout << HELP << endl;
 		return 1;
 	}
 
-	Mat bin_img;
 	Mat res_img;
 	Mat img = imread(argv[1]);
-
 	if( !img.data )
 		return 2;
 
-	cvtColor(img, img, CV_BGR2GRAY);
-	namedWindow(testW, WINDOW_NORMAL);
-	imshow(testW, img);
-	resizeWindow(testW, 600, 600);
-	
-	threshold(img, bin_img, 128, 255, 1);
-	imshow(testW, bin_img);
-	
+	preProcess(img, res_img); 
 
-	/* erodeToSingle(bin_img, res_img, 500); */
-	
-	fd_gui fg(bin_img, 600, 600, testW);
-	createTrackbar("Descriptor Count", testW, &fg.desc_count, fg.max_d, showFdGui, &fg);
+	// TODO: Insert ShapeD code here
+
+	namedWindow(fdWin, WINDOW_NORMAL);
+	resizeWindow(fdWin, 600, 600);
+		
+	fd_gui fg(res_img, 600, 600, fdWin);
+	createTrackbar("Descriptor Count", fdWin, &fg.desc_count, fg.max_d, showFdGui, &fg);
 
 	waitKey(0);
 	return 0;
